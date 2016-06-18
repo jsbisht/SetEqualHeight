528429cf2f74008b2e53f0b2c2da549c055b2312
Jagdeep Singh Bisht <jagdeepbisht89@gmail.com>
2016-06-18 11:06:12 +0530

    Add files via upload

---
+++ setequalheight.js
@@ -0,0 +1,29 @@
+angular.module('homepage').
+/**
+ * @param 'main' CSS Selector for element whose height is to be set on other elements
+ * @param 'others' Array of CSS Selector of elements whose height is to be set
+ * @param 'offset' Any variance required from the height being set to the element
+ */
+directive('setequalheight', function($timeout) {
+  return {
+    link: setequalheightLinker,
+    restrict: 'A',
+    replace: false
+  }
+
+  function setequalheightLinker(scope, element, attribute) {
+    $timeout(function () {
+      scope.$apply(function(){
+        var params = JSON.parse(attribute.setequalheight);
+        var mainElement = element[0].querySelector(params.main);
+        var mainElementHeight = mainElement.offsetHeight;
+        for(var i=0; i<params.others.length; i++) {
+          var otherElement = element[0].querySelector(params.others[i]);
+          if(otherElement && otherElement != null) {
+            otherElement.style.height = (mainElementHeight - params.offset[i]) + 'px';
+          }
+        }
+      });
+   },1200);
+ }
+});
